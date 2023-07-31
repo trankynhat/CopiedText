c0f615c1ec126e2eb789ccecd3dff612e04f3d31
trankynhat <nhattranking@gmail.com>
2023-07-31 23:19:50 +0700

    done

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <div>
+    
+    <my-component></my-component>
+  </div>
+</template>
+
+
+
+<script>
+import MyComponent from '~/components/MyComponent.vue';
+
+
+
+export default {
+  components: {
+    MyComponent
+  }
+}
+</script>
